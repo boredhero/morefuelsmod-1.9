70b334d81607f216b8df0d317391dc3eb64d7291
boredherobrine13 <treeclan12@gmail.com>
2016-03-15 22:22:47 -0400

    modified:   .gitignore 	new file:   CREDITS-fml.txt 	new file:   LICENSE-fml.txt 	new file:   MinecraftForge-Credits.txt 	new file:   MinecraftForge-License.txt 	new file:   Paulscode IBXM Library License.txt 	new file:   Paulscode SoundSystem CodecIBXM License.txt 	new file:   README.txt 	new file:   build.gradle 	new file:   forge-1.8.9-11.15.1.1764-changelog.txt 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   src/main/java/com/bored/morefuelsmod/Fuels.java 	new file:   src/main/java/com/bored/morefuelsmod/Main.java 	new file:   src/main/resources/mcmod.info

---
+++ src/main/java/com/bored/morefuelsmod/Main.java
@@ -0,0 +1,43 @@
+package com.bored.morefuelsmod;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.common.config.Configuration;
+
+
+@Mod(modid = Main.MODID, version = Main.VERSION, name=Main.MODNAME)
+public class Main {
+
+	public static final String MODID = "morefuels";
+	public static final String VERSION = "1.2.0";
+	public static final String MODNAME = "More Fuels Mod";
+	
+	@Instance(value = Main.MODID)
+    public static Main instance;
+	
+	@EventHandler
+	public void preinit(FMLPreInitializationEvent event){
+		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+		config.load();
+		boolean enableRFtLrecipe = config.get(config.CATEGORY_GENERAL, "enableRFtLrecipe", true).getBoolean(true);
+		if(enableRFtLrecipe)
+			GameRegistry.addSmelting(Items.rotten_flesh, new ItemStack(Items.leather), 0.3F);
+		config.save();
+		//I feel like I need an "else if" statement here telling it to not add the recipe if it doesnt return true.
+		//Also, how to I get the actual config file to appear......like do I make it or is it generated and if so how
+		//and do i need to type any code for that...
+		}
+
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event){
+		GameRegistry.registerFuelHandler(new Fuels());
+		}
+}
