19ab7447a0655b2c12f39ae6817db1891be8ea90
boredherobrine13 <treeclan12@gmail.com>
2016-03-15 23:50:21 -0400

    Updated version to from 1.2.0 to 1.2.1-DEV in main.java

--- build/tmp/expandedArchives/retromappedReplacedMain.jar_3wn4p1ahz8lqclqn0mlaotcgr/com/bored/morefuelsmod/Main.java
+++ build/tmp/expandedArchives/retromappedReplacedMain.jar_3wn4p1ahz8lqclqn0mlaotcgr/com/bored/morefuelsmod/Main.java
@@ -1,6 +1,5 @@
 package com.bored.morefuelsmod;
 
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
@@ -26,13 +25,12 @@ public class Main {
 	public void preinit(FMLPreInitializationEvent event){
 		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
 		config.load();
-		boolean enableRFtLrecipe = config.get(config.CATEGORY_GENERAL, "enableRFtLrecipe", true).getBoolean(true);
+		boolean enableRFtLrecipe = config.get(Configuration.CATEGORY_GENERAL, "enableRFtLrecipe", true).getBoolean(true);
 		if(enableRFtLrecipe)
 			GameRegistry.addSmelting(Items.field_151078_bh, new ItemStack(Items.field_151116_aA), 0.3F);
 		config.save();
 		//I feel like I need an "else if" statement here telling it to not add the recipe if it doesnt return true.
 		//Also, how to I get the actual config file to appear......like do I make it or is it generated and if so how
-		//and do i need to type any code for that...
 		}
 
 	
--- src/main/java/com/bored/morefuelsmod/Main.java
+++ src/main/java/com/bored/morefuelsmod/Main.java
@@ -15,7 +15,7 @@ import net.minecraftforge.common.config.Configuration;
 public class Main {
 
 	public static final String MODID = "morefuels";
-	public static final String VERSION = "1.2.0";
+	public static final String VERSION = "1.2.1-DEV";
 	public static final String MODNAME = "More Fuels Mod";
 	
 	@Instance(value = Main.MODID)
