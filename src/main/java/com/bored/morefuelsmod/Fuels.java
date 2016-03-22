ca46b0399cb1c81ee7c00da18e1db2c0bff54003
boredherobrine13 <treeclan12@gmail.com>
2016-03-21 23:47:33 -0400

    Fixed boat handling procedure for 1.9 so that all boat types are burnable, not just oak.

--- src/main/java/com/bored/morefuelsmod/Fuels.java
+++ src/main/java/com/bored/morefuelsmod/Fuels.java
@@ -172,9 +172,26 @@ public class Fuels implements IFuelHandler{
 				//Actual calculation here was 225 but I rounded.
 					return 240;
 				}
+				//Updated boat handler to 1.9 to include different woods.
 				if (fuel.getItem() == Items.boat){
 					return 1500;
 				}
+				if (fuel.getItem() == Items.acacia_boat){
+					return 1500;
+				}
+				if (fuel.getItem() == Items.birch_boat){
+					return 1500;
+				}
+				if (fuel.getItem() == Items.dark_oak_boat){
+					return 1500;
+				}
+				if (fuel.getItem() == Items.jungle_boat){
+					return 1500;
+				}
+				if (fuel.getItem() == Items.spruce_boat){
+					return 1500;
+				}
+				//End of new boat handling procedure
 				if (fuel.getItem() == Items.reeds){
 					return 300;
 				}
