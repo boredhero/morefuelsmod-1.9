318549debbba303b984e9e967b742ea87c046d6f
boredherobrine13 <treeclan12@gmail.com>
2016-03-21 23:42:03 -0400

    Added ghast tears.

--- src/main/java/com/bored/morefuelsmod/Fuels.java
+++ src/main/java/com/bored/morefuelsmod/Fuels.java
@@ -267,6 +267,9 @@ public class Fuels implements IFuelHandler{
 				if (fuel.getItem() == Items.magma_cream){
 					return 2000;
 				}
+				if (fuel.getItem() == Items.ghast_tear){
+					return 2000;
+				}
 				//Attempt to implement spawn eggs as being burnable...idk why.
 				if (fuel.getItem() == Items.spawn_egg){
 					return 5000;
