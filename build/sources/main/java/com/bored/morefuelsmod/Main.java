04ed86d727fe51cda792baa5566f97d7f59e1ef4
boredherobrine13 <treeclan12@gmail.com>
2016-04-17 11:15:41 -0400

    Compiled Version 1.3.2 Release for MC 1.9

--- build/sources/main/java/com/bored/morefuelsmod/Main.java
+++ build/sources/main/java/com/bored/morefuelsmod/Main.java
@@ -17,7 +17,7 @@ import com.bored.morefuelsmod.Fuels;
 public class Main {
 
 	public static final String MODID = "morefuelsmod-1.9";
-	public static final String VERSION = "1.3.1";
+	public static final String VERSION = "1.3.2";
 	public static final String MODNAME = "More Fuels Mod";
 	
 	@Instance(value = Main.MODID)
